e7062c56c1cba5d166fa303ca31148d6e17a2cd9
Paul Cabeza <ovidio.cabeza@gmail.com>
2021-09-18 07:14:18 -0600

    registration form added

--- user/views.py
+++ user/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import forms
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import MyCustomUserCreationForm
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'user/register.html', {'form': form})
+    if request.method == 'POST':
+        form = MyCustomUserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('index')
+
+    else:
+        form = MyCustomUserCreationForm()
+
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'user/register.html', context)
